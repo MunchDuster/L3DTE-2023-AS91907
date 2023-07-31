0b201636c3f1274a2955d48eab03a2ea723438b8
Malachai <72060614+MunchDuster@users.noreply.github.com>
2023-07-31 21:32:09 +1200

    Update Buzzer.ino

--- Arduino code/Buzzer/Buzzer.ino
+++ Arduino code/Buzzer/Buzzer.ino
@@ -1,16 +1,31 @@
-// Piezo Speaker test
-// beeps buzzer at 440Hz (A4 note)
-// Tutorial link: http://www.ardumotive.com/how-to-use-a-buzzer-en.html
+// Piezo Speaker test : Plays a little song in A major
+// Tutorial link: http://www.ardumotive.com/how-to-use-a-buzzerPin-en.html
 
-const int buzzer = 9; //buzzer to arduino pin 9
+//Note: Buzzer should have at least 100 ohm resistor from arduino into positive terminal
+const int buzzerPin = 2;
 
+//Song constants
+const int noteGap = 30;
+const int noteDuration = 100;
+const int notes[] = {1, 5, 8, 12, 17, 12, 8, 5}; //Notes in bar
+const int bars[] = {0, -5, 0, -7}; //Bars in song
+  
 void setup(){
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(buzzerPin, OUTPUT); // Set buzzerPin as an output
 }
 
 void loop(){
-  tone(buzzer, 440); // Send sound signal...
-  delay(1000);        // ...for 1 sec
-  noTone(buzzer);     // Stop sound...
-  delay(1000);        // ...for 1sec
+  for(int b = 0; b < 4; b++) {
+    for(int n = 0; n < 8; n++) {
+      playNote(notes[n] + bars[b]);
+    }
+  }
+}
+
+void playNote(int n) {
+  double frequency = 440.0 * pow(pow(2.0, 1.0/12.0), n); // frequency of note in half steps from A4
+  tone(buzzerPin, (int)frequency); //Play the sound to the nearest integer frequency
+  delay(noteDuration);
+  noTone(buzzerPin); //Quiet
+  delay(noteGap);
 }
