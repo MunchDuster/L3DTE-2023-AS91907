3123b911b1b1a94f2f9e4da902e3f3fd8ff184fa
Malachai <72060614+MunchDuster@users.noreply.github.com>
2023-08-18 11:18:29 +1200

    New accelerometer test code, simplified and elegant

--- Arduino code/Accelerometer/Accelerometer.ino
+++ Arduino code/Accelerometer/Accelerometer.ino
@@ -1,75 +1,31 @@
-//MPU6050 accelerometer test 3
-//PID controller on the direction of the car to face the target direction
+//Deliverable 2 MPU6050 code test
+//Simplified using some peer review for the most basic and reliable system
 
-//Wiring:
-//Arduino - MPU6050
-//A5 - SCL/SDA
-//A4 - SDA/SCL
-//5V - VCC
-//GND - GND
+#include <MPU6050.h>
 
-#include "MPU6050.h"
+//Constants
+const double minimumReading = -32768; //Lowest reading from sernsor
+const double maximumReading = 32767; //Highest reading from sensor
+const double gyroRangeDegreesPerSecond = 250; //The highest reading from sensor in degrees per second
+const int deltaTimeMilliseconds = 10; //Milliseconds between iterations
+const double deltaTimeSeconds = (double)deltaTimeMilliseconds / 1000.0;
 
-//Controls - Tuning required
-const double proportionalFactor = 200;
-const double integratedFactor = 50;
-const double derivativeFactor = 700;
-const double refreshRateHertz = 100;
-const double targetAngleDegrees = 90;
-const int calibrationReadings = 200;
-const int calibrationTimeMilliseconds = 1000;
-const int accelerometerTestConnectionFailDelayMilliseconds = 500;
-
-//Constants (Not for control)
-const double gyroScaleFactor = 1.0 / 131.0; //Magic number from who knows where
-double deltaTimeSeconds = 1.0 / refreshRateHertz;
-int deltaTimeMilliseconds = (int)(deltaTimeSeconds * 1000.0);
-//Main vars
+//Variables
 MPU6050 accelgyro;
-int16_t rawGyroZ; //Z angle is yaw
-double deltaAngleDegrees, deltaAngleDegreesOffset;
-double angleDegrees, error, outputPID, proportionalValue, integratedValue, derivativeValue;
+int16_t ax, ay, az, gx, gy, gz;
+double deltaAngleDegreesPerSecond, angleDegrees;
 
+//Functions
 void setup() {
-  //Initialize serial
-  Serial.begin(9600);
-
-  //Initialize MPU6050
-  Wire.begin();
-  accelgyro.initialize();
-  while(!accelgyro.testConnection()) {
-    Serial.println("MPU6050 connection failed");
-    delay(accelerometerTestConnectionFailDelayMilliseconds);
-  }
-  Serial.println("MPU6050 connection successful");
-
-  //Calibrate MPU6050
-  for(int i = 0;i < calibrationReadings; i++) {
-    deltaAngleDegreesOffset += GetDeltaAngle(false); 
-    delay(deltaTimeMilliseconds);
-  }
-  deltaAngleDegreesOffset /= (double) calibrationReadings;
+  Serial.begin(9600); //Initialize serial
+  accelgyro.initialize(); //Initialize MPU6050
+  Serial.println(accelgyro.testConnection() ? "MPU Connection Success" : "MPU Connection FAIL");
 }
 
 void loop() {
-  //Update angle
-  GetDeltaAngle(false);
-  angleDegrees += deltaAngleDegrees;
-
-  //PID control
-  error = targetAngleDegrees - angleDegrees; //Offset from target
-  derivativeValue = -deltaAngleDegrees * derivativeFactor; //Slow down to prevent overshoot
-  proportionalValue = error * proportionalFactor; //Pull towards target
-  integratedValue += error * integratedFactor; //Improve accuracy on target
-  outputPID = derivativeValue + proportionalValue + integratedValue;
-
-  //Output and delay
-  Serial.println(angleDegrees);
-  //Serial.println(outputPID);
-  delay(deltaTimeMilliseconds);
-}
-double GetDeltaAngle(bool useOffset) {
-  rawGyroZ = accelgyro.getRotationZ(); //Read value from MPU6050
-  deltaAngleDegrees = (double)rawGyroZ  * gyroScaleFactor * deltaTimeSeconds; //Convert to degrees/sec
-  if(useOffset) deltaAngleDegrees -= deltaAngleDegreesOffset;
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz); //Read MPU6050 values
+  deltaAngleDegreesPerSecond = map((double)gz, minimumReading, maximumReading, -gyroRangeDegreesPerSecond, gyroRangeDegreesPerSecond); //Scale value from output to degrees per second
+  angleDegrees += deltaAngleDegreesPerSecond * deltaTimeSeconds; //Add delta to accumulate
+  Serial.println(angleDegrees); //Log to serial
+  delay(deltaTimeMilliseconds); //Wait for next iteration
 }
