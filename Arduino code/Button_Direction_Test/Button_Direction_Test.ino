10a0005abedb1650cf3832f94845893dde716a1d
Malachai <72060614+MunchDuster@users.noreply.github.com>
2023-08-20 18:19:47 +1200

    Update Button_Direction_Test.ino

--- Arduino code/Button_Direction_Test/Button_Direction_Test.ino
+++ Arduino code/Button_Direction_Test/Button_Direction_Test.ino
@@ -1,26 +1,78 @@
+//Five button test
 
-#define leftButtonPin 4
-#define rightButtonPin 5
-#define upButtonPin 3
-#define downButtonPin 6
+
+#include <List.hpp>
+
+const int BUTTON_CHECK_DELAY = 30;
+
+const int leftButton = 3;
+const int startButton = 4;
+const int rightButton = 5;
+const int forwardButton = 6;
+const int backButton = 7;
+
+//Variables for storing instructions
+enum Instruction { EMPTY, FORWARD, BACKWARD, LEFT, RIGHT } ;
+List<Instruction> instructions;
+
+//Variables for button inputs
+Instruction pendingInstruction = EMPTY;
+Instruction lastInstruction = EMPTY;
+bool pendingStart;
 
 void setup() {
-  pinMode(rightButtonPin, INPUT_PULLUP);
-  pinMode(leftButtonPin, INPUT_PULLUP);
-  pinMode(upButtonPin, INPUT_PULLUP);
-  pinMode(downButtonPin, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
+  pinMode(leftButton, INPUT_PULLUP);
+  pinMode(forwardButton, INPUT_PULLUP);
+  pinMode(backButton, INPUT_PULLUP);
+  pinMode(startButton, INPUT_PULLUP);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  bool rightValue = !digitalRead(rightButtonPin);
-  bool leftValue = !digitalRead(leftButtonPin);
-  bool upValue = !digitalRead(upButtonPin);
-  bool downValue = !digitalRead(downButtonPin);
-  Serial.print(rightValue);
-  Serial.print(leftValue);
-  Serial.print(upValue);
-  Serial.println(downValue);
-  delay(10);
+  GetInput();
+  Serial.println("RUNNING (NOT REALLY)");
+  delay(1000);
+}
+
+void GetInput() {  
+  Serial.println("GETTING INPUT");
+  while(!pendingStart) {
+    //Get input
+    CheckButtons();
+    //Add input to instructions
+    if(pendingInstruction != EMPTY) {
+      instructions.add(pendingInstruction);
+      lastInstruction = pendingInstruction;
+      pendingInstruction = EMPTY;
+    }
+    delay(BUTTON_CHECK_DELAY);
+  }
+  pendingStart = false;
+  pendingInstruction = EMPTY;
+}
+
+void CheckButtons() {
+  //Wait for last button to be released
+  if(lastInstruction != EMPTY) {
+    int checkButton;
+    switch(lastInstruction) {
+      case FORWARD: checkButton = forwardButton; break;
+      case BACKWARD: checkButton = backButton; break;
+      case LEFT: checkButton = leftButton; break;
+      case RIGHT: checkButton = rightButton; break;
+    }
+    //Check the button is not being pressed
+    if(digitalRead(checkButton) == HIGH) {
+      lastInstruction = EMPTY;
+    }
+    else return;
+  }
+  //Because buttons are pullup, LOW is when pressed
+  if(digitalRead(forwardButton) == LOW) {pendingInstruction = FORWARD; Serial.println("FORWARD PRESSED"); }
+  else if(digitalRead(backButton) == LOW) {pendingInstruction = BACKWARD; Serial.println("BACKWARD PRESSED"); }
+  else if(digitalRead(leftButton) == LOW) {pendingInstruction = LEFT; Serial.println("LEFT PRESSED"); }
+  else if(digitalRead(rightButton) == LOW) {pendingInstruction = RIGHT; Serial.println("RIGHT PRESSED"); }
+  else if(digitalRead(startButton) == LOW) {pendingStart = true; Serial.println("START PRESSED"); }
 }
